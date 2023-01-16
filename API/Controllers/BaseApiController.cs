f89eec3f50a1d068ca540c98f10ffb5e68ecd487
Shahed Kaiser <shahedkaiser@hotmail.com>
2023-01-16 20:56:56 +0600

    code uploaded to source control

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // localhost/api/users
+
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
