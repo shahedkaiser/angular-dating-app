fed847a305477b1a85d6fbebc2157c1f1f66b395
Shahed Kaiser <shahedkaiser@hotmail.com>
2023-01-17 12:20:24 +0600

    displayed all members in card and implemented jtw token interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { SharedModule } from './_modules/shared.module';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 
 @NgModule({
@@ -29,7 +31,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     ListsComponent,
     MessagesComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -40,7 +43,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -11,23 +11,15 @@ export class HomeComponent implements OnInit{
   registerMode=false;
   users:any;
 
-  constructor(private http:HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.getUsers();
+    
   }
 
   registerToggle()
   {
     this.registerMode=!this.registerMode;
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next : response => this.users = response,
-      error : error => console.log(error),
-      complete : () => console.log('request has been completed')
-    })
   }
 
   cancelRegisterMode(event:boolean){
