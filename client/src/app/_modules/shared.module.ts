fbc4c73f6557516abb9dae63ea6c5373856804a5
Shahed Kaiser <shahedkaiser@hotmail.com>
2023-01-18 13:11:07 +0600

    Added member details page

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 
 
@@ -10,6 +11,7 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     ToastrModule.forRoot(
       {
         positionClass: 'toast-top-center'
@@ -17,7 +19,8 @@ import { ToastrModule } from 'ngx-toastr';
   ],
   exports:[
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }
